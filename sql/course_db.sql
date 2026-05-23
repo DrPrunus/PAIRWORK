b51242010ec8c649c4c0c2e061af15edffe0ec2c
delicious421 <18841051896@163.com>
2026-05-23 15:43:25 +0800

    Create course_db if not exists and drop student table
    修正数据库

--- sql/course_db.sql
+++ sql/course_db.sql
@@ -8,7 +8,6 @@
 CREATE DATABASE IF NOT EXISTS `course_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE `course_db`;
-
 -- =============================================
 -- 1. 学生表
 -- =============================================
